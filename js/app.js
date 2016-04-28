4f5d52d6bd918de806b6b9982c81a72bf5ccde0a
zhanfang <277208607@qq.com>
2016-04-28 23:34:36 +0800

    完成todomvc基本功能及路由功能，下一步进行可编辑功能实现

--- js/app.js
+++ js/app.js
@@ -1,11 +1,46 @@
 (function(exports) {
   'use strict';
+  var filters = {
+    all: function(todos) {
+      return todos;
+    },
+    active: function(todos) {
+      return todos.filter(function(todo) {
+        return !todo.completed;
+      });
+    },
+    completed: function(todos) {
+      return todos.filter(function(todo) {
+        return todo.completed;
+      });
+    }
+  };
   exports.app = new Vue({
     el: '.todoapp',
     data: {
       todos: todoStorage.fetch(),
       newTodo: 'this is init',
       visibility: 'all'
+    },
+    computed: {
+      filteredTodos: function() {
+        return filters[this.visibility](this.todos);
+      },
+      remaining: function() {
+        return filters.active(this.todos).length;
+      },
+      allDone: {
+        get: function() {
+          console.log('get');
+          return this.remaining === 0;
+        },
+        set: function(value) {
+          console.log('set');
+          this.todos.forEach(function(todo) {
+            todo.completed = value;
+          })
+        }
+      }
     },
     watch: {
       handler: function() {
@@ -22,6 +57,9 @@
           completed: false
         });
         this.newTodo = '';
+      },
+      removeTodo: function(todo) {
+        this.todos.$remove(todo);
       }
     }
   });
